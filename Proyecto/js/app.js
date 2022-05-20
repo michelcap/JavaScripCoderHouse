f3d7bb0b3e57feb9ba372d181eaa62cbfefb7e1f
michelCoderHouse <m.s.capurro.delbono@gmail.com>
2022-05-20 18:41:07 -0500

    Agregado de Arreglos

--- Proyecto/js/app.js
+++ Proyecto/js/app.js
@@ -1,3 +1,9 @@
+/// arreglos que contendran los ingreses egresos que se carguen o eliminen
+const ingresos = [new Ingreso('Salario', 3000), new Ingreso('Auto', 6000)];
+const egresos = [new Egreso('Alquiler', 3000), new Egreso('Gym', 500)];
+
+
+/// funcion que suma el total de los ingresos recorriendo el arreglo ingresos
 let totalIngresos = () => {
 	let totalIngreso = 0;
 	for (let ingreso of ingresos) {
@@ -6,6 +12,7 @@ let totalIngresos = () => {
 	return totalIngreso;
 };
 
+/// funcion que suma el total de los ingresos recorriendo el arreglo egresos
 let totalEgresos = () => {
 	let totalEgreso = 0;
 	for (let egreso of egresos) {
@@ -14,8 +21,14 @@ let totalEgresos = () => {
 	return totalEgreso;
 };
 
+/// da formato al valor para que adopte el estilo de moneda
 const formatoMoneda = (valor) => {
 	return valor.toLocaleString('es-UY', {style: 'currency', currency: 'UYU', minimumFractionDigits: 2});
+};
+
+/// da formato al porcentaje que representa en egresos
+const formatoPorcentaje = (valor) => {
+	return valor.toLocaleString('es-UY', {style: 'percent', minimumFractionDigits: 2});
 };
 
 
