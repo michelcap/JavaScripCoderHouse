683ff628fb0b3843b44332e9ac8a809425f459b6
michelCoderHouse <m.s.capurro.delbono@gmail.com>
2022-06-18 07:10:25 -0500

    Fetch

--- Proyecto/js/appi.js
+++ Proyecto/js/appi.js
@@ -4,21 +4,56 @@ const convertirMoneda = () => {
 	const form = document.forms['formulario_API'];
 	const from = form['from'];
 	const to = form['to'];
-    const valor_from = form['valor_from'];
-	if (from.value !== '' && to.value !== '') {
-		urll = `https://api.fastforex.io/convert?from=${from.value}&to=${to.value}&amount=${valor_from.value}&api_key=215841912e-f429b5a8a1-rdnmee`
+	const valor_from = form['valor_from'];
+
+	if (from.value !== '' && to.value !== '' && valor_from.value > 0 && !valor_from.value.includes("-")) {
+
+		async function newsSearch(from, to, value) {
+			let url = `https://api.fastforex.io/convert?from=${from}&to=${to}&amount=${value}&api_key=215841912e-f429b5a8a1-rdnmee`;
+			const resp = await fetch(url);
+			const info = await resp.json();
+			return info;
+		}
+
+		let info = newsSearch(from.value, to.value, valor_from.value);
+		info
+			.then(response => {
+				let moneda = to.value;
+				console.log(moneda);
+				let retorno = response.result.USD;				
+				console.log(response);
+				document.getElementById('valor_to').innerHTML = retorno;
+			})
+			.catch(error => {
+				swal({
+					title: "Error",
+					text: "Lo ingresado en el campo moneda no corresponde a un formato valido. Ejemplo: Dolares Americanos es USD. Puede consultar la siguiente referencia https://es.iban.com/currency-codes",
+					icon: "error",
+					button: "Corregir",
+				});
+			});
 	}
-    console.log(urll)
+	else if (valor_from.value === '' || valor_from.value === "0") {
+		swal({
+			title: "Error",
+			text: "Campo valor vacio o incorrecto",
+			icon: "error",
+			button: "Corregir",
+		});
+	}
+	else if (valor_from.value.includes("-")) {
+		swal({
+			title: "Error",
+			text: "Campo valor no admite signos",
+			icon: "error",
+			button: "Corregir",
+		});
+	}
+
+
 };
 let checkBTN_api = document.getElementById("click_btn_api");
-checkBTN_api.addEventListener("click", convertirMoneda );
-/*"https://api.fastforex.io/convert?from=EUR&to=UYU&amount=50&api_key=215841912e-f429b5a8a1-rdnmee"*/
-// const options = {method: 'GET', headers: {Accept: 'application/json'}};
-
-// fetch(urll, options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
+checkBTN_api.addEventListener("click", convertirMoneda);
 
 /// evita que se recargue la paguina por activar cargar el formulario
 const noReloadAPI = (e) => {
