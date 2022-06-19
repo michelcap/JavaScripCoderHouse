00f85c1edcb6508fb4817963842b76fc55300ced
michelCoderHouse <m.s.capurro.delbono@gmail.com>
2022-06-18 19:35:38 -0500

    Fetch en tu proyecto

--- Proyecto/js/appi.js
+++ Proyecto/js/appi.js
@@ -1,21 +1,24 @@
 let urll = "";
 /// funcion que interactua con API para convertir la moneda
 const convertirMoneda = () => {
+	/// toma del formulario los datos de entradas ingresados por el usuario
 	const form = document.forms['formulario_API'];
 	const from = form['from'];
 	const to = form['to'];
 	const valor_from = form['valor_from'];
 
+	/// evalua algunas condiciones para evitar que el servidor devuelva error
 	if (from.value !== '' && to.value !== '' && valor_from.value > 0 && !valor_from.value.includes("-")) {
-
+		/// solicita a la api los datos usando carga por url
 		async function newsSearch(from, to, value) {
 			let url = `https://api.fastforex.io/convert?from=${from}&to=${to}&amount=${value}&api_key=215841912e-f429b5a8a1-rdnmee`;
 			const resp = await fetch(url);
 			const info = await resp.json();
 			return info;
 		}
-
+		/// carga en info la respuesta del servidor
 		let info = newsSearch(from.value, to.value, valor_from.value);
+		/// completa la promesa
 		info
 			.then(response => {
 				let retorno = Object.values(response.result);		
@@ -49,6 +52,7 @@ const convertirMoneda = () => {
 
 
 };
+/// escucha al boton de conversion de moneda
 let checkBTN_api = document.getElementById("click_btn_api");
 checkBTN_api.addEventListener("click", convertirMoneda);
 
